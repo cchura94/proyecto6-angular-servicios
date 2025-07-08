721522d33763f6e196435bc327433f68e40413ea
cristian <cchura.cpc@gmail.com>
2025-07-07 23:09:13 -0400

    chatOPENAI Servicios FormModule

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,21 +1,69 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Signal, signal } from '@angular/core';
 import { ChatService } from './chat.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css'
 })
 export class Chat implements OnInit{
 
-  chatService = inject(ChatService);
+  mensaje:string = '';
+  respuesta: string = ''
+
+  preguntaSignal = signal({
+    tipo: 'user',
+    mensaje: '',
+    respuesta: ''
+  })
+  preguntas: Signal<{tipo: string,
+                    mensaje: string,
+                    respuesta: string}[]> = signal([]);
+
+  // chatService = inject(ChatService);
+  constructor(private chatService: ChatService){
+    console.log("CONSTRUCTOR")
+    // this.obtenerRespuestaOpenAI();
+  }
 
   ngOnInit(){
-    this.chatService.consultarOpenAiApi("Que es Node?").subscribe((result:any) => {
-     console.log(result.choices[0].message.content);
+    console.log("ngOnInit")
+  }
+
+  obtenerRespuestaOpenAI(){
+    this.preguntaSignal().tipo = 'user';
+    this.preguntaSignal().mensaje = this.preguntaSignal().mensaje;
+    this.preguntas().push({
+      mensaje: this.preguntaSignal().mensaje,
+      respuesta: '',
+      tipo: 'user'})
+
+    this.chatService.consultarOpenAiApi(this.preguntaSignal().mensaje).subscribe((result:any) => {
+      // console.log(result.choices[0].message.content);
+      // this.respuesta = result.choices[0].message.content
+    
+
+      this.preguntaSignal.set({
+        mensaje: this.preguntaSignal().mensaje,
+        respuesta: result.choices[0].message.content,
+        tipo: 'ia'})
+
+      this.preguntas().push({
+        mensaje: this.preguntaSignal().mensaje,
+        respuesta: result.choices[0].message.content,
+        tipo: 'ia'})
+
+      
+      // this.respuesta = result.choices[0].message.content
     })
-     
-  }  
+
+     this.preguntaSignal().mensaje = '';
+       this.preguntaSignal().tipo = '';
+       this.preguntaSignal().respuesta = '';
+
+     console.log(this.preguntas())
+  }
 
 }
